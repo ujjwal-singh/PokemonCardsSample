ba64f53363a1877843f6eb2f080c0ea3364506cc
ujjwal-singh <ujj.1729@gmail.com>
2017-11-25 13:52:26 +0530

    Added username validity info button

--- app/src/main/java/com/example/ujjwal/pokemoncardssample/MainActivity.java
+++ app/src/main/java/com/example/ujjwal/pokemoncardssample/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.ujjwal.pokemoncardssample;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
@@ -66,6 +69,9 @@ public class MainActivity extends AppCompatActivity
     /** Google API Client for Google sign-in. */
     private GoogleApiClient mGoogleApiClient;
 
+    /** Username validity information button. */
+    private Button usernameValidityInfoButton;
+
     /**
      *  This is the method which is called upon activity creation.
      *  @param savedInstanceState
@@ -79,6 +85,8 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(myToolbar);
 
         signUpUsername = (TextView) this.findViewById(R.id.signUpUsername);
+        usernameValidityInfoButton = (Button) this.findViewById(R.id.
+                usernameValidityInfo);
 
         signInButton = (SignInButton) this.findViewById(R.id.signInButton);
         signUpButton = (SignInButton) this.findViewById(R.id.signUpButton);
@@ -396,9 +404,34 @@ public class MainActivity extends AppCompatActivity
                         GOOGLE_SIGN_UP_REQUEST_CODE);
                 break;
 
+            case R.id.usernameValidityInfo:
+                usernameValidityDialog();
+                break;
+
             default:
                 break;
         }
+    }
+
+    /**
+     *  This method displays a dialog containing information
+     *  about validity of username.
+     */
+    private void usernameValidityDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.usernameHintDialogTitle)
+                .setMessage(R.string.usernameHintDialogMessage)
+                .setPositiveButton(R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(final DialogInterface dialog,
+                                                final int which) {
+
+                            }
+                        });
+
+        builder.show();
     }
 
     /**
